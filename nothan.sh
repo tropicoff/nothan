4e5e4e361fbf0fb33f937dfa619a780eeb2dad4b
tropicoff <tropic.officiel15@gmail.com>
2025-09-13 21:05:19 +0000

    mdofif minime de clear a la fin des menu

--- nothan.sh
+++ nothan.sh
@@ -3,14 +3,14 @@
 #menu  nothan
 CHOICE=$(whiptail --title "nothan" --menu "Choisis une option :" 15 50 3 \
 "1" "configuration" \
-"2" "Option 2" \
+"2" "instalation logiciel" \
 "3" "Quitter" 3>&1 1>&2 2>&3)
 
 # Gestion du choix
 case $CHOICE in
-    1) echo "lancement de la config nothan";;
-    2) sudo apt install apache2 && sudo apt update sudo apt install php -y sudo && apt install avahi-daemon && systemctl enable avahi-daemon ;;
-    3) echo "Fermeture du menu" ;;
+    1) echo "lancement de la config nothan" && clear;;
+    2) sudo apt install apache2 && sudo apt update sudo apt install php -y sudo && apt install avahi-daemon && systemctl enable avahi-daemon && clear ;;
+    3) echo "Fermeture du menu" && clear ;;
     *) echo "Choix invalide" ;;
 esac
 clear
