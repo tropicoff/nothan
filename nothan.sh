4b8614cb40f86ab5feca36573877f26065862b8e
tropicoff <tropic.officiel15@gmail.com>
2025-09-24 16:38:14 +0200

    Update nothan.sh

--- nothan.sh
+++ nothan.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-#menu  nothan
-CHOICE=$(whiptail --title "nothan" --menu "Choisis une option :" 15 50 3 \
-"1" "configuration" \
-"2" "instalation logiciel" \
-"3" "mise a jour logiciel" \
+# Menu nothan
+CHOICE=$(whiptail --title "nothan" --menu "Choisis une option :" 15 50 4 \
+"1" "Configuration" \
+"2" "Installation logiciel" \
+"3" "Mise à jour logiciel" \
 "4" "Quitter" 3>&1 1>&2 2>&3)
 
 # Gestion du choix
 case $CHOICE in
-    1) echo "lancement de la config nothan" && clear;;
-    2) sudo apt install apache2 && sudo apt install php -y && sudo apt install avahi-daemon && systemctl enable avahi-daemon && clear ;;
-    3) cd && cd $HOME/nothan/ sudo rm -r /var/www/html/* && git clone https://github.com/tropicoff/nothan.git && clear ;;
-    4) exit
-    *) echo "Choix invalide" ;;
+    1) 
+        echo "Lancement de la configuration nothan..."
+        sleep 2
+        clear
+        ;;
+    2) 
+        echo "Installation des paquets nécessaires..."
+        sudo apt update -y
+        sudo apt install -y apache2 php avahi-daemon
+        sudo systemctl enable avahi-daemon
+        clear
+        ;;
+    3) 
+        echo "Mise à jour de nothan..."
+        sudo rm -rf /var/www/html/*
+        git clone https://github.com/tropicoff/nothan.git /tmp/nothan
+        sudo cp -r /tmp/nothan/* /var/www/html/
+        rm -rf /tmp/nothan
+        clear
+        ;;
+    4) 
+        echo "Au revoir 👋"
+        exit 0
+        ;;
+    *) 
+        echo "Choix invalide" 
+        ;;
 esac
+
 clear
